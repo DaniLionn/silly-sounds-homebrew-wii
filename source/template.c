7d172f56d484ffa53d7c0b32ec873a1d623bd454
DaniLionn <107291896+DaniLionn@users.noreply.github.com>
2024-10-29 19:14:18 -0600

    v1.1

--- source/template.c
+++ source/template.c
@@ -20,6 +20,22 @@
 #include "sound7_mp3.h"
 #include "sound8_mp3.h"
 #include "sound9_mp3.h"
+#include "sound10_mp3.h"
+#include "sound11_mp3.h"
+#include "sound12_mp3.h"
+#include "sound13_mp3.h"
+#include "sound14_mp3.h"
+#include "sound15_mp3.h"
+#include "sound16_mp3.h"
+#include "sound17_mp3.h"
+#include "sound18_mp3.h"
+#include "sound19_mp3.h"
+#include "sound20_mp3.h"
+#include "sound21_mp3.h"
+#include "sound22_mp3.h"
+#include "sound23_mp3.h"
+#include "sound24_mp3.h"
+#include "sound25_mp3.h"
 
 static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
@@ -96,6 +112,23 @@ int main(int argc, char **argv) {
 		if ( pressed & WPAD_BUTTON_DOWN ) 	MP3Player_PlayBuffer(sound7_mp3, sound7_mp3_size, NULL);
 		if ( pressed & WPAD_BUTTON_LEFT ) 	MP3Player_PlayBuffer(sound8_mp3, sound8_mp3_size, NULL);
 		if ( pressed & WPAD_BUTTON_RIGHT ) 	MP3Player_PlayBuffer(sound9_mp3, sound9_mp3_size, NULL);
+		if ( pressed & WPAD_NUNCHUK_BUTTON_C ) 	MP3Player_PlayBuffer(sound10_mp3, sound10_mp3_size, NULL);
+		if ( pressed & WPAD_NUNCHUK_BUTTON_Z ) 	MP3Player_PlayBuffer(sound11_mp3, sound11_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_A) 	MP3Player_PlayBuffer(sound12_mp3, sound12_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_B) 	MP3Player_PlayBuffer(sound13_mp3, sound13_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_X) 	MP3Player_PlayBuffer(sound14_mp3, sound14_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_Y) 	MP3Player_PlayBuffer(sound15_mp3, sound15_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_PLUS) 	MP3Player_PlayBuffer(sound16_mp3, sound16_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_MINUS) 	MP3Player_PlayBuffer(sound17_mp3, sound17_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_FULL_L) 	MP3Player_PlayBuffer(sound18_mp3, sound18_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_FULL_R) 	MP3Player_PlayBuffer(sound19_mp3, sound19_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_ZL) 	MP3Player_PlayBuffer(sound20_mp3, sound20_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_ZR) 	MP3Player_PlayBuffer(sound21_mp3, sound21_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_UP) 	MP3Player_PlayBuffer(sound22_mp3, sound22_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_DOWN) 	MP3Player_PlayBuffer(sound23_mp3, sound23_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_LEFT) 	MP3Player_PlayBuffer(sound24_mp3, sound24_mp3_size, NULL);
+		if ( pressed & WPAD_CLASSIC_BUTTON_RIGHT) 	MP3Player_PlayBuffer(sound25_mp3, sound25_mp3_size, NULL);
+
 		// Wait for the next frame
 		VIDEO_WaitVSync();
 	}
